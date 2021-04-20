6358f1b06c3b83ec5394ba358a109b993e991be1
Nazar Shcherbiy <Chay23@users.noreply.github.com>
2021-04-20 15:37:28 +0300

    Add endpoint to create user and profile together

--- app/users/managers.py
+++ app/users/managers.py
@@ -4,6 +4,33 @@ from django.core.validators import validate_email
 
 
 class UserManager(BaseUserManager):
+    def create_with_profile(self, email: str, password: str, first_name: str, last_name: str, group_id: int):
+        from .models import UserProfile
+        from study_groups.models import StudyGroup
+        """
+        Creates, saves and return a User with appropriate UserProfile
+        :param email:
+        :param password:
+        :return:
+        """
+        user = self.model(
+            email=self.normalize_email(email),
+        )
+        user.set_password(password)
+        user.is_student = True
+        user.save(using=self._db)
+
+        group = StudyGroup.objects.get(id=group_id)
+
+        profile = UserProfile(
+            user=user,
+            first_name=first_name,
+            last_name=last_name,
+            group=group
+        )
+        profile.save()
+        return user
+
     def create_user(self, email: str, password: str = None) -> User:
         """
         Creates, saves and returns a User.
